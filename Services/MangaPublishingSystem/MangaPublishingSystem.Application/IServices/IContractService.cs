f965ec6fe7e4d332d084b0caf148db379005ddd2
SonNPXSE183201 <SonNPXSE183201@fpt.edu.vn>
2026-06-13 22:48:59 +0700

    refactor: define IContractService signatures returning DTO

--- Services/MangaPublishingSystem/MangaPublishingSystem.Application/IServices/IContractService.cs
+++ Services/MangaPublishingSystem/MangaPublishingSystem.Application/IServices/IContractService.cs
@@ -1,8 +1,12 @@
 using MangaPublishingSystem.Domain.Entities;
+using MangaPublishingSystem.Application.DTOs.Contract;
 
 namespace MangaPublishingSystem.Application.IServices
 {
     public interface IContractService : IGenericService<Contract>
     {
+        System.Threading.Tasks.Task<ContractDto> CreateContractAsync(CreateContractDto dto);
+        System.Threading.Tasks.Task AcceptContractAsync(int contractId, int mangakaId);
+        System.Threading.Tasks.Task DeclineContractAsync(int contractId, int mangakaId, string declineReason);
     }
 }
