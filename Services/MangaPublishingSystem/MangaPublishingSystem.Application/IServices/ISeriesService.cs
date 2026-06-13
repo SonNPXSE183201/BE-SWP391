19028974424fe8ebfdc3702c8ca56d1d4dac47ed
SonNPXSE183201 <SonNPXSE183201@fpt.edu.vn>
2026-06-13 22:49:09 +0700

    refactor: define ISeriesService signatures returning DTOs for series workflow

--- Services/MangaPublishingSystem/MangaPublishingSystem.Application/IServices/ISeriesService.cs
+++ Services/MangaPublishingSystem/MangaPublishingSystem.Application/IServices/ISeriesService.cs
@@ -7,9 +7,13 @@ namespace MangaPublishingSystem.Application.IServices
 {
     public interface ISeriesService : IGenericService<Series>
     {
-        Task<Series> CreateSeriesAsync(int mangakaId, CreateSeriesDto createDto);
+        Task<SeriesDto> CreateSeriesAsync(int mangakaId, CreateSeriesDto createDto);
         System.Threading.Tasks.Task SubmitForReviewAsync(int seriesId, int mangakaId, SubmitSeriesReviewDto submitDto);
         System.Threading.Tasks.Task SetAbsenceStatusAsync(int mangakaId, bool onLeave);
-        Task<IEnumerable<Series>> GetSeriesByMangakaIdAsync(int mangakaId);
+        Task<IEnumerable<SeriesDto>> GetSeriesByMangakaIdAsync(int mangakaId);
+        System.Threading.Tasks.Task SubmitDraftManuscriptAsync(int seriesId, int mangakaId, string draftManuscriptUrl);
+        System.Threading.Tasks.Task EvaluateSeriesByEditorAsync(int seriesId, int editorId, EditorEvaluationDto dto);
+        System.Threading.Tasks.Task CastBoardVoteAsync(int seriesId, int boardMemberId, BoardVoteDto dto);
+        System.Threading.Tasks.Task FinalizeBoardDecisionAsync(int seriesId, BoardDecisionDto dto);
     }
 }
