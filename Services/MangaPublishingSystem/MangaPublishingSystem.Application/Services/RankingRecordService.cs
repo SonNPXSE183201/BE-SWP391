49a57ef938cdf2d0876a547db355529f4bc19b29
SonNPXSE183201 <SonNPXSE183201@fpt.edu.vn>
2026-06-17 11:05:36 +0700

    feat(application): implement application logic and business rules

--- Services/MangaPublishingSystem/MangaPublishingSystem.Application/Services/RankingRecordService.cs
+++ Services/MangaPublishingSystem/MangaPublishingSystem.Application/Services/RankingRecordService.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using MangaPublishingSystem.Domain.Entities;
 using MangaPublishingSystem.Application.IRepositories;
 using MangaPublishingSystem.Application.IServices;
+using MangaPublishingSystem.Application.DTOs.Rankings;
 
 namespace MangaPublishingSystem.Application.Services
 {
@@ -8,6 +13,29 @@ namespace MangaPublishingSystem.Application.Services
     {
         public RankingRecordService(IRankingRecordRepository repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
         {
+        }
+
+        public async System.Threading.Tasks.Task CreateRankingsAsync(CreateRankingsDto dto)
+        {
+            var orderedRecords = dto.Records.OrderByDescending(r => r.VoteCount).ToList();
+            for (int i = 0; i < orderedRecords.Count; i++)
+            {
+                var record = orderedRecords[i];
+                var rankingRecord = new RankingRecord
+                {
+                    SeriesId = record.SeriesId,
+                    VoteCount = record.VoteCount,
+                    RankPosition = i + 1,
+                    RecordedDate = dto.RecordedDate
+                };
+                await _repository.AddAsync(rankingRecord);
+            }
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<RankingRecord>> GetRankingsByPeriodAsync(DateTime period)
+        {
+            return await _repository.FindAsync(r => r.RecordedDate.Date == period.Date);
         }
     }
 }
