fd7d8d2eba6efe72a33bb72af1f81dab31af8f05
SonNPXSE183201 <SonNPXSE183201@fpt.edu.vn>
2026-07-11 02:26:57 +0700

    feat: add script to download and extract U-Net model weights for image colorization

--- Services/AiVisionService/download_unet.py
+++ Services/AiVisionService/download_unet.py
@@ -8,7 +8,7 @@ gdown.download(id="1qmxUEKADkEM4iYLp1fpPLLKnfZ6tcF-t", output="manga_colorizatio
 
 # Download denoiser
 print("Downloading denoiser...")
-gdown.download(id="161oyQcYpdkVdw8gKz_MA8RD-Wtg9XDp3", output="manga_colorization/denoising/models/denoiser.pth", quiet=False)
+gdown.download(id="161oyQcYpdkVdw8gKz_MA8RD-Wtg9XDp3", output="manga_colorization/denoising/models/net_rgb.pth", quiet=False)
 
 print("Extracting generator.zip...")
 with zipfile.ZipFile("manga_colorization/networks/generator.zip", 'r') as zip_ref:
